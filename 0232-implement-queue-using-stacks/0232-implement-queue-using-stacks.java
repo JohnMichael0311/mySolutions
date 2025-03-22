4f72a0f5a79a1f74dfec8c04b16ab4c85a03fe5b
John Michael <142437273+JohnMichael0311@users.noreply.github.com>
2025-03-23 00:59:58 +0530

    Time: 0 ms (100%), Space: 41.5 MB (37.15%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -7,29 +7,47 @@ class MyQueue {
     }
     
     public void push(int x) {
-        
-        while(!s1.isEmpty()){
-            s2.push(s1.peek());
-            s1.pop();
-        }
+        //approach 1
+        // while(!s1.isEmpty()){
+        //     s2.push(s1.peek());
+        //     s1.pop();
+        // }
+        // s1.push(x);
+        // while(!s2.isEmpty()){
+        //     s1.push(s2.peek());
+        //     s2.pop();
+        // }
+
+        // approach 2
         s1.push(x);
-        while(!s2.isEmpty()){
-            s1.push(s2.peek());
-            s2.pop();
-        }
     }
     
     public int pop() {
-       return s1.pop();
+    //    return s1.pop();
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+            s2.push(s1.pop());
+            
+            }
+        } 
+        return s2.pop();
     }
     
     public int peek() {
-        return s1.peek();
+        // return s1.peek();
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+            s2.push(s1.pop());
+           
+            }
+        } 
+        return s2.peek();
     }
     
     public boolean empty() {
-        if(s1.size()==0) return true;
-        else return false;
+        // if(s1.size()==0) return true;
+        // else return false;
+        return s1.isEmpty() && s2.isEmpty();
     }
 }
 
