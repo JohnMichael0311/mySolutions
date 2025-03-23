bb71184b4e16c192991ac0281c71a560675f485b
John Michael <142437273+JohnMichael0311@users.noreply.github.com>
2025-03-24 02:42:01 +0530

    Time: 4 ms (97.24%), Space: 44.9 MB (53.92%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,35 +1,55 @@
-class Pair {
-    int x, y;
-    Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
+// class Pair {
+//     int x, y;
+//     Pair(int x, int y) {
+//         this.x = x;
+//         this.y = y;
+//     }
+// }
 class MinStack {
 
-    private Stack<Pair> st;
+    private Stack<Long> st;
+    private long mini;
     public MinStack() {
         st = new Stack<>();
+        mini = Integer.MAX_VALUE;
     }
     
     public void push(int val) {
+        // if(st.isEmpty()){
+        //     st.push(new Pair(val, val));
+        // }else {
+        //     st.push(new Pair(val , Math.min(val, st.peek().y)));
+        // }
+        // Approach 2
         if(st.isEmpty()){
-            st.push(new Pair(val, val));
-        }else {
-            st.push(new Pair(val , Math.min(val, st.peek().y)));
+            mini = val;
+            st.push((long)val);
+        } 
+        else if(val >= mini) {
+            st.push((long)val);
+        }
+        else {
+            st.push((2L *val) - mini);
+            mini = val;
         }
     }
     
     public void pop() {
-        st.pop();
+        long x = st.pop();
+        if(x<mini){
+            mini = (2*mini) - x;
+        }
+        
     }
     
     public int top() {
-        return st.peek().x;
+        long y = st.peek();
+        if(mini<y) return (int)y;
+        else return (int)mini;
     }
     
     public int getMin() {
-        return st.peek().y;
+        return (int)mini;
     }
 }
 
